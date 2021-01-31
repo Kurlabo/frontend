8ba7f5210354a8e775209770c44748ea5402e493
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-01-31 19:28:45 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <div class="bg-white dark:bg-black">
+        <h1 class="text-gray-900 dark:text-white">Dark mode</h1>
+        <p class="text-gray-500 dark:text-gray-300">The feature you've all been waiting for.</p>
+      </div>
     </div>
   );
 }
