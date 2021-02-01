4f13c643de34dcec9cef2cd21b44ee425ffb0116
SangEon Park <sangeon1500@daum.net>
2021-02-01 14:52:27 +0900

    config env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kp: {
+          100: '#B1A9C6',
+          200: '#D2BEFF',
+          300: '#BA99E1',
+          400: '#A775D6',
+          500: '#875EB3',
+          600: '#5F0080',
+        },
+        discount: {
+          100: '#FA622F',
+        },
+        kdp: {
+          100: '#31123D',
+          200: '#401351',
+          300: '#4F1266',
+          400: '#5E107B',
+        },
+        klp: {
+          100: '#E5D6E8',
+          200: '#CBAFD2',
+          300: '#B088BC',
+          400: '#9563A6',
+          500: '#7A3D91',
+          600: '#5E107B',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
