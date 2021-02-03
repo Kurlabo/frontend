4e64eb44eefe49e9abab0c9a19a551a54a865fbc
EOJIN KIM <kimeojin1995@gmail.com>
2021-02-03 11:25:08 +0900

    git merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { Route } from 'react-router';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import FindIdPage from './pages/FindIdPage';
+import FindPasswordPage from './pages/FindPasswordPage';
 import MainContainer from './container/MainContainer';
-
 function App() {
   return (
-    <div>
+    <Switch>
       <Route path="/" exact component={MainContainer} />
-    </div>
+      <Route path="/shop/account/signin" component={LoginPage} />
+      <Route path="/shop/account/find_id" component={FindIdPage} />
+      <Route path="/shop/account/find_pwd" component={FindPasswordPage} />
+    </Switch>
   );
 }
 
