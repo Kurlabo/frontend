03d156d0a0b0a30fd9a657ff4c1d8008a37c2ab1
FE-sophie <saeyoung19@gmail.com>
2021-02-09 00:33:35 +0900

    feat:인풋 유효성 검사에 따른 모달팝업 화면에 띄우기 기능 구현

--- src/containers/auth/FindIdForm.js
+++ src/containers/auth/FindIdForm.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import FindId from '../../components/login/FindId';
+import Modalform from '../../components/login/Modalform';
+import Modaltest from '../../components/login/Modaltest';
 
 export default function FindIdForm() {
-  return <FindId />;
+  const findUserId = '';
+
+  return <FindId onSubmit={onSubmit} findUserId={findUserId} />;
+  function onSubmit(e) {
+    //비동기 작업
+    e.preventDefault();
+  }
 }
