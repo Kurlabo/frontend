88571ca0a5acae7a6bf37fdbdd0471cb7395abe3
FE-sophie <saeyoung19@gmail.com>
2021-02-09 00:17:15 +0900

    styl: tailwind config 정의 및 스타일수정

--- src/components/login/Input.jsx
+++ src/components/login/Input.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // const AuthStyledInput = styled.input``;
 export default function Input(props) {
   return (
     <input
       {...props}
-      className="text-1.4 rounded inline-block w-340 h-54 px-19 border border-gray-300 mt-4 Default focus style focus:bg-klp-700"
+      className="text-r-1.4 rounded inline-block w-p-340 h-p-54 px-r-1.9 border border-gray-300 mt-4 Default focus style focus:bg-klp-700"
     />
   );
 }
