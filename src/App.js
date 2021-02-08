8cafd762a3c16b6e66b82844cfc486c8cd954a3e
FE-sophie <saeyoung19@gmail.com>
2021-02-09 00:39:32 +0900

    feat:모달 라우트 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import Modal from './components/login/Modal';
 import FindIdPage from './pages/FindIdPage';
 import FindPasswordPage from './pages/FindPasswordPage';
 import MainContainer from './container/MainContainer';
@@ -13,6 +14,7 @@ function App() {
       <Route path="/shop/account/find_id" component={FindIdPage} />
       <Route path="/shop/account/find_pwd" component={FindPasswordPage} />
       <Route path="/shop/goods/item_list" component={ItemListPage} />
+      <Route path="/shop/account/modal" component={Modal} />
       <Route path="/" exact component={MainContainer} />
       <Route component={NotFoundPage} />
     </Switch>
