7ee0f1bb941c7d39a3e373079cd4d3e8d06508bc
NaraBaek <nara130825@gmail.com>
2021-02-24 01:01:51 +0900

    fix: 장바구니 담기 알림 모달창의 긴 상품명 줄바꿈되는 현상 수정
    - display: -webkit-box을 이용하여 elipsis 구현

--- src/components/header/AddCartModal.jsx
+++ src/components/header/AddCartModal.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const NameBox = styled.p`
+  overflow: hidden;
+  width: 100%;
+
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+`;
 
 const AddCartModalStyle =
   'absolute border bg-white border-gray-200 p-7 text-r-1.6 w-r-35.5 h-r-10.3 right-72 top-r-7.4 z-50';
@@ -16,7 +26,9 @@ const AddCartModal = () => {
       <div className="flex justify-between">
         <img className="w-r-4.6 h-r-6" src={itemDetail && itemDetail.list_image_url} alt="img" />
         <div className="text-r-1.4 font-medium w-r-24 flex flex-col justify-around">
-          <p className="text-gray-500">{itemDetail && itemDetail.name}</p>
+          <div className="py-1 w-full">
+            <NameBox className="text-p-14 text-gray-500">{itemDetail && itemDetail.name}</NameBox>
+          </div>
           <p>장바구니에 상품을 담았습니다.</p>
           {isNew === 'addCnt' && <p>이미 담으신 상품이 있어 추가되었습니다.</p>}
         </div>
