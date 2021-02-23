2893729272948202c4d0ca3b6857bbe93be79c33
FE-sophie <saeyoung19@gmail.com>
2021-02-23 14:37:16 +0900

    fix: 비밀번호 변경 성공여부 props로 전달하여 모달띄워줌

--- src/containers/auth/FindPasswordForm.jsx
+++ src/containers/auth/FindPasswordForm.jsx
@@ -3,8 +3,11 @@ import FindPassword from '../../components/login/FindPassword';
 
 export default function FindPasswordForm() {
   const open = true;
+  const changeSuccess = true;
   const value = '사용자 정보가 존재하지 않습니다';
-  return <FindPassword onSubmit={onSubmit} open={open} value={value} />;
+  return (
+    <FindPassword onSubmit={onSubmit} open={open} value={value} changeSuccess={changeSuccess} />
+  );
   function onSubmit(e) {
     //비동기 작업
     e.preventDefault();
