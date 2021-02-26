878105964040cbd75f397ba8e8f06cf4218a99e6
FE-sophie <saeyoung19@gmail.com>
2021-02-26 11:48:50 +0900

    feat:늘사는것 담기 API 작업 완료

--- src/components/mykurly/MyWishList.jsx
+++ src/components/mykurly/MyWishList.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MyKurlyHeader from './MyKurlyHeader';
 import MyKurlyCategory from './MyKurlyCategory';
 import MyKurlyPageNation from './MyKurlyPageNation';
 import CheckBox from '../login/CheckBox';
+import CartModal from '../itemList/CartModal';
+import { useSelector, useDispatch } from 'react-redux';
+import { getWishItems } from '../../modules/wishList';
+import MyWishListItem from './MyWishListItem';
+import { withRouter, useParams } from 'react-router-dom';
+
 const MyWishList = () => {
   return (
     <>
@@ -15,7 +21,18 @@ const MyWishList = () => {
   );
 };
 
-const MyWishListBlock = () => {
+const MyWishListBlock = withRouter(({ history }) => {
+  const [modalIsOpen, setmodalIsOpen] = useState(false);
+  const [allCheck, setAllCheck] = useState(false);
+  const [checkList, setCheckList] = useState([]);
+  const [cartItem, setCartItem] = useState('');
+
+  const wishList = useSelector(state => state.wish.data);
+  const QueryString = history.location.search;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    QueryString ? dispatch(getWishItems(QueryString)) : dispatch(getWishItems());
+  }, [QueryString]);
   return (
     <div className="float-left align-middle w-r-85 h-full mt-20 mb-6 px-12 pb-32 box-border">
       <h1 className="a11y-hidden">배송지 확인</h1>
@@ -30,54 +47,44 @@ const MyWishListBlock = () => {
           <li>
             <ul className="text-r-1.4 pb-8 border-b font-medium border-kg-300 box-border leading-none">
               <li className="inline-block text-center align-middle">
-                <CheckBox />
+                <CheckBox
+                  id="allCheck"
+                  checkAll={checkAll}
+                  allCheck={allCheck}
+                  setAllCheck={setAllCheck}
+                />
               </li>
               <li className="w-r-62 text-center inline-block align-middle">상품정보</li>
               <li className="w-48 text-center inline-block align-middle">선택</li>
             </ul>
           </li>
-          <MyWishListItem />
+          <MyWishListItem openModal={openModal} wishList={wishList} allCheck={allCheck} />
         </ul>
         <div className="text-right">
           <button className="mt-8 text-r-1.3 border border-kp-600 text-kp-600 py-4 px-10">
             늘 사는 것 비우기
           </button>
         </div>
-        <MyKurlyPageNation />
+        <MyKurlyPageNation
+          pageNumber={wishList.totalPages}
+          totalProduct={wishList.totalElements}
+          elementNumber={wishList.numberOfElements}
+          history={history}
+        />
       </div>
+      <CartModal modalIsOpen={modalIsOpen} closeModal={closeModal} cartItem={cartItem} />
     </div>
   );
-};
+  function openModal(e) {
+    setmodalIsOpen(true);
+    setCartItem(e.target.name);
+  }
+  function closeModal() {
+    setmodalIsOpen(false);
+  }
+  function checkAll(e) {
+    setAllCheck(!allCheck);
+  }
+});
 
-const MyWishListItem = () => {
-  return (
-    <li className="border-b border-kg-80 box-border">
-      <ul className="text-r-1.4 ">
-        <li className="inline-block text-center align-middle">
-          <CheckBox />
-        </li>
-        <li className="clear-fix px-8 w-r-62 h-full text-center inline-block align-middle ">
-          <div className="float-left w-36 py-8 ">
-            <img src="https://img-cf.kurly.com/shop/data/goods/1582679290125s0.jpg" alt="" />
-          </div>
-          <div className="pl-24 py-8 float-left text-left w-r-48 text-kg-300">
-            <h2 className="text-r-1.6 w-full font-medium truncate">
-              [남향푸드또띠아] 간편 간식 브리또 8종
-            </h2>
-            <p className="text-r-1.2 my-7">4,700원</p>
-            <p className="text-r-1.2 font-medium text-kp-600">※패키지 구성품</p>
-            <p className="text-r-1.2 mt-1 w-fulltruncate">바질10g/1개/1800원</p>
-          </div>
-        </li>
-        <li className="w-48 text-center inline-block align-middle">
-          <button className="text-r-1.3 py-2 w-40 mb-2 border border-kp-600 bg-kp-550 text-white">
-            장바구니담기
-          </button>
-          <button className="text-r-1.3 border w-40 py-2 border-kp-600 text-kp-600">삭제</button>
-        </li>
-      </ul>
-    </li>
-  );
-};
-
-export default MyWishList;
+export default withRouter(MyWishList);
---
+++ src/modules/api/apiWishList.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+// 위시리스트 목록을 가져오는 비동기 함수
+export const getWishItems = async (requestPage = '?page=0') => {
+  console.log('위시리스트', requestPage);
+  return await axios.get(`http://3.35.221.9:8080/api/mypage/mypage_wishlist${requestPage}`);
+};
+
+export const deleteWishItems = async () => {
+  return await axios.delete('http://3.35.221.9:8080/api/mypage/mypage_wishlist');
+};
+
+// export const deleteWishItems = async idArray => {
+//     return await axios.delete('http://3.35.221.9:8080/api/mypage/mypage_wishlist', {
+//       product_id: idArray,
+//     });
+//   };
