29c9e8554026884f87e56a3dc957f3aad6c516f9
FE-sophie <saeyoung19@gmail.com>
2021-03-03 15:14:53 +0900

    fix: console.log( )코드 삭제

--- src/modules/api/apiWishList.js
+++ src/modules/api/apiWishList.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 // 위시리스트 목록을 가져오는 비동기 함수
 export const getWishItems = async (requestPage = '?page=0') => {
-  console.log('위시리스트', requestPage);
   return await axios.get(`http://3.35.221.9:8080/api/mypage/mypage_wishlist${requestPage}`);
 };
 
